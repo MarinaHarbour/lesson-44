6db92adcd23792557ae19a7748294dce9c5be48e
MarinaHarbour <shunatovamarina@gmail.com>
2024-11-02 18:47:03 +0200

    Add files via upload

--- module_10_1.py
+++ module_10_1.py
@@ -4,21 +4,29 @@ import time
 
 
 def write_words(word_count, file_name):
-    start = time.time()
+    # start = time.time()
     with open(file_name, 'w', encoding='utf=8') as file:
         for i in range(1, word_count + 1):
             file.write(f"Какое-то слово № {i}\n")
             sleep(0.1)
-    end = time.time()
-    time_ = end - start
+    # end = time.time()
+    # time_ = end - start
     print(f"Завершилась запись в файл {file_name}")
-    print(f"Работа потоков {time_}")
+    # print(f"Работа потоков {time_}")
 
+
+start = time.time()
 
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
+
+end = time.time()
+time_ = end - start
+print(f"Работа потоков {time_}")
+
+start_2 = time.time()
 
 thread = threading.Thread(target=write_words, args=(10, 'example5.txt'))
 thread_2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
@@ -34,3 +42,7 @@ thread.join()
 thread_2.join()
 thread_3.join()
 thread_4.join()
+
+end_2 = time.time()
+time_2 = end_2 - start_2
+print(f"Работа потоков {time_2}")
